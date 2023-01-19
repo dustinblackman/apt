8e24b775747a335f9b430922db7fc03ed34d1115
Dustin Blackman <dev@dustinblackman.com>
2023-01-19 09:05:56 -0500

    chore: add new folder structure

--- add-debs.sh
+++ add-debs.sh
@@ -9,28 +9,27 @@ DISTFOLDER="$3"
 docker build -t apt-deploy:local .
 
 cat _redirects | grep 'https' | awk '{print $2}' | while read link; do
-	curl -L -o "deb/files/$(basename "$link")" "$link"
+	curl -L -o "pool/main/$(basename "$link")" "$link"
 done
 
 ls "$DISTFOLDER" | grep '.deb' | while read file; do
-	echo "/deb/files/${file} https://github.com/dustinblackman/${APP}/releases/download/v${VERSION}/${file} 302" >>_redirects
+	echo "/pool/main/${file} https://github.com/dustinblackman/${APP}/releases/download/v${VERSION}/${file} 302" >>_redirects
 done
 
-cd deb
-cp -r "$DISTFOLDER"/*.deb ./files/
+cp -r "$DISTFOLDER"/*.deb ./pool/main/
 
-rm -f ./Packages
-docker run -it -v "$PWD:/deb" apt-deploy:local bash -c "cd deb && dpkg-scanpackages --multiversion ./files > Packages"
-gzip -k -f Packages
+rm -rf all-packages dists
+docker run -it -v "$PWD:/project" apt-deploy:local bash -c "cd project && dpkg-scanpackages --multiversion ./pool/ > all-packages"
+cat all-packages | grep 'Architecture:' | awk -F ': ' '{print $2}' | sort | uniq | while read arch; do
+	mkdir -p "dists/stable/main/binary-${arch}"
+	docker run -t -v "$PWD:/project" apt-deploy:local bash -c "cd project && dpkg-scanpackages --arch ${arch} pool/ > dists/stable/main/binary-${arch}/Packages"
+	gzip -k -f "dists/stable/main/binary-${arch}/Packages"
+done
+rm -f all-packages
 
-rm -f ./Release
-docker run -it -v "$PWD:/deb" apt-deploy:local bash -c "cd deb && apt-ftparchive release . > Release"
-
-rm -f ./Release.gpg
-gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" -abs -o - Release >Release.gpg
-
-rm -f ./InRelease
-gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" --clearsign -o - Release >InRelease
+docker run -it -v "$PWD:/project" apt-deploy:local bash -c "cd project && apt-ftparchive release . > dists/stable/Release"
+gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" -abs -o - Release >dists/stable/Release.gpg
+gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" --clearsign -o - Release >dists/stable/InRelease
 
 git add .
 git commit -m "add $APP $VERSION"
