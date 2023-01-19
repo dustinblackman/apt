6e8ab66762f971d5da5abee6953c1cb7d679aeb2
Dustin Blackman <dev@dustinblackman.com>
2023-01-19 07:02:10 -0500

    fix: switch to _redirects file

--- add-debs.sh
+++ add-debs.sh
@@ -8,29 +8,13 @@ DISTFOLDER="$3"
 
 docker build -t apt-deploy:local .
 
-cat links | grep 'https' | while read link; do
+cat _redirects | grep 'https' | awk '{print $2}' | while read link; do
 	curl -L -o "deb/files/$(basename "$link")" "$link"
 done
 
 ls "$DISTFOLDER" | grep '.deb' | while read file; do
-	echo "https://github.com/dustinblackman/${APP}/releases/download/v${VERSION}/${file}" >>links
+	echo "/deb/files/${file} https://github.com/dustinblackman/${APP}/releases/download/v${VERSION}/${file} 302" >>_redirects
 done
-
-rm "functions/deb/files/[file].js"
-cat >"functions/deb/files/[file].js" <<-EOM
-	const links = \`
-	$(cat links)
-	\`;
-
-	export function onRequest(context) {
-	  const link = links.split('\n').filter(e => e).find(e => e.includes(context.params.file));
-	  if (!link) {
-	    return new Response(\`\${context.params.file} not found.\`, { status: '404' });
-	  }
-
-	  return Response.redirect(context.params.file, 302);
-	}
-EOM
 
 cd deb
 cp -r "$DISTFOLDER"/*.deb ./files/
@@ -48,6 +32,6 @@ gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" -abs -o - Release >
 rm -f ./InRelease
 gpg --default-key "6A34CFEE77FE8257C3BB92FE24C3FC5D6987904B" --clearsign -o - Release >InRelease
 
-# git add .
-# git commit -m "add $APP $VERSION"
-# git push
+git add .
+git commit -m "add $APP $VERSION"
+git push
